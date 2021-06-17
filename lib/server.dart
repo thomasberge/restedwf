b5895c43fb4f781f5e21eb1b1175711982d241b7
Thomas Berge <46525121+thomasberge@users.noreply.github.com>
2021-06-17 10:02:26 +0200

    Update server.dart

--- lib/server.dart
+++ lib/server.dart
@@ -1,1 +1,24 @@
+import 'dart:io';
+import 'dart:convert';
+import 'rested.dart';
 
+RestedRequestHandler rested;
+main() async {
+  RestedServer server = new RestedServer();
+  await server.start();
+  server.keepAlive();
+}
+
+class Rested extends RestedRequestHandler {
+  Rested() {
+    this.address = "0.0.0.0";
+    this.port = 8080;
+    this.addResource(Resource_root(), "/");
+  }
+}
+
+class Resource_root extends RestedResource {
+  void get(RestedRequest request) {
+    request.response(type: "file", filepath: "index.html");
+  }
+}
