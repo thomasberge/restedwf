a5aa3c42b2be9d4fa6dfa565273e39ba73fa72b6
thomasberge <thomas.berge@lyse.net>
2022-02-16 13:53:11 +0100

    Small bugfix.

--- lib/src/errors.dart
+++ lib/src/errors.dart
@@ -2,7 +2,7 @@ class Errors {
 
     static Map<String, dynamic> getJson(int code) {
 
-        print("-- HTTP STATUS " + code.toString + " --");
+        print("-- HTTP STATUS " + code.toString() + " --");
         Map<String, dynamic> message = {};
         message["code"] = code;
 
