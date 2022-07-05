5d113b2ee7ee9fa59d634cd400fcc450e63df01c
thomasberge <thomas.berge@lyse.net>
2022-07-05 14:18:26 +0200

    Updating source.

--- lib/src/contenttype.dart
+++ lib/src/contenttype.dart
@@ -67,6 +67,7 @@ Future<RestedRequest> receive_content(RestedRequest request) async {
 Future<RestedRequest> receive_application_json(RestedRequest request) async {
     String jsonstring = await utf8.decoder.bind(request.request).join();
     request.raw = jsonstring;
+    print("1: " + jsonstring.toString());
     
     // dirty trick to manually change a json sent as string to a parsable string. Unelegant af
     Map jsonmap = {};
@@ -90,7 +91,7 @@ Future<RestedRequest> receive_application_json(RestedRequest request) async {
           jsonmap = jsonmap['body'];
         }        
     }
-
+        print("2: " + jsonmap.toString());
       request.setBody(jsonmap);
     return request;
 }
