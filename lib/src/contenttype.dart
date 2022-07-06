b60cf7a5a342213d4b0d6e711f2ddc18b38e19ae
thomasberge <thomas.berge@lyse.net>
2022-07-06 06:35:01 +0200

    Updating source.

--- lib/src/contenttype.dart
+++ lib/src/contenttype.dart
@@ -53,8 +53,6 @@ Map<String, Function> content_types = {
 };
 
 Future<RestedRequest> receive_content(RestedRequest request) async {
-    //String jsonstring = await utf8.decoder.bind(request.request).join();
-    print("0:" + jsonstring.toString());
     List<String> temp = request.request.headers.contentType.toString().split(';');
     String type = temp[0].toString();
     if(content_types.containsKey(type)) {
